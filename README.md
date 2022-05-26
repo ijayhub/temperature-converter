c64a7bd0b9846d8a82d05e65b2c5bb1a0d697404
symplyijay <ijeonyi@gmail.com>
2022-05-26 18:00:21 +0100

    temperature-converter-app

---
+++ README.md
@@ -0,0 +1,41 @@
+<div id="top"></div>
+<div align="center">
+  
+  <h1>Temperature Converter App</h1>
+</div>
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+        <a href="#about-the-project">About The Project</a>
+        <ul>
+          <li><a href="#built-with">Built With</a></li>
+          <li><a href="#Running-this-project">Running this project<a></li>
+        </ul>
+    </li>      
+  </ol>
+</details>
+
+Repo_name: 
+
+Hosted link: 
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+### Built With
+```
+In building this project i used the following : 
+> HTML
+> CSS (Media Query, FLex box)
+> JavaScript
+> Live sever to preview the project locally on my computer
+> Text Editor was visual studio code.
+```
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+### Running this project
+
+Create a project directory and it should be your necessary files(html,css,javascript).
+
+<p align="right">(<a href="#top">back to top</a>)</p>
---
+++ app.js
@@ -0,0 +1,51 @@
+const celsiusInput = document.querySelector('#celsius');
+const fahrenheitInput = document.querySelector('#fahrenheit');
+const kelvinInput = document.querySelector('#kelvin');
+const tempInputs = document.querySelectorAll('input');
+
+
+
+tempInputs.forEach(function (input) {
+    input.addEventListener('input', function (e) {
+
+        let tempValue = parseInt(e.target.value);
+        let inputName = e.target.name;
+
+        if (e.target.value === '') {
+            celsiusInput.value = null;
+            fahrenheitInput.value = null;
+            kelvinInput.value = null;
+
+            return;
+        }
+
+
+        if (inputName === "celsius") {
+            // celsius to fahrenheit
+            let fahrenheitValue = tempValue * 1.8 + 32;
+            fahrenheitInput.value = fahrenheitValue.toFixed(2)
+            // celsius to kelvin
+            let kelvinValue = tempValue + 273.15
+            kelvinInput.value = kelvinValue.toFixed(2)
+        } else if (inputName === "fahrenheit") {
+            // fahrenheit to celsius
+            let celsiusValue = (tempValue - 32) / 1.8
+            celsiusInput.value = celsiusValue.toFixed(2)
+            // fahrenheit to kelvin
+            let kelvinValue = (tempValue - 32) / 1.8 + 273.15
+            kelvinInput.value = kelvinValue.toFixed(2)
+        }
+        else if (inputName === "kelvin") {
+            // kelvin to celsius
+            let celsiusValue = tempValue - 273.15
+            celsiusInput.value = celsiusValue.toFixed(2)
+            // kelvin to fahrenheit
+            let fahrenheitValue = (tempValue - 273.15) * 1.8 + 32
+            fahrenheitInput.value = fahrenheitValue.toFixed(2)
+            
+            
+            
+        }
+    })
+}) 
+    
